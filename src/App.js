a3c696c41c0f75d22c7fbc5aac230c8fe1b7db06
Aruna Jayaweera <it21210006@my.sliit.lk>
2024-03-14 20:18:57 +0530

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+// import axios 
+import axios from 'axios';
+
+// import icons
+import { 
+  IoMdSunny, 
+  IoMdRainy, 
+  IoMdCloudy, 
+  IoMdSnow, 
+  IoMdThunderstorm, 
+  IoMdSearch
+} from 'react-icons/io';
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind
+} from 'react-icons/bs';
+
+import { TbTemperatureCelsius} from 'react-icons/tb';
+import { ImSpinner8} from 'react-icons/im';
+
+// api key
+const APIkey = 'c096698d8315f53e530941dd7eb03999'
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState('Bucharest');
+
+  // fetch the data
+  useEffect(()=> {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+
+    axios.get(url).then(res => {
+      setData(res.data);
+    });
+  }, [location]);
+
+  // if data is false show the loader
+  if (!data) {
+    return (
+      <div>
+        <div>
+          <ImSpinner8 className='text-5xl animate-spin' />
+        </div>
+      </div>
+    )
+  }
+
   return <div>react app</div>;
 };
 
