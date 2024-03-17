7dddf166af5890746246768f58cff8337ad101f1
Aruna Jayaweera <it21210006@my.sliit.lk>
2024-03-17 12:54:42 +0530

    animate button

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
   const [data, setData] = useState(null);
   const [location, setLocation] = useState('Bucharest');
   const [inputValue, setInputValue] = useState('');
+  const [animate, setAnimate] = useState(false);
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
@@ -49,6 +50,16 @@ const App = () => {
 
     //select input
     const input = document.querySelector('input');
+
+    //if input value is empty
+    if(input.value === '') {
+      //set aniate to true
+      setAnimate(true);
+      //after 500ms set animate to false
+      setTimeout(()=>{
+        setAnimate(false);
+      }, 500)
+    }
 
     //clear input
     input.value='';
@@ -115,7 +126,11 @@ const App = () => {
     bg-no-repeat bg-cover bg-center flex flex-col
     items-center justify-center px-4 lg:px-0'>
       {/* form */}
-      <form className='h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8'>
+      <form 
+        className={`${animate ? 'animate-shake' : 'animate-none'
+        } h-16 bg-black/30 w-full max-w-[450px] 
+        rounded-full backdrop-blur-[32px] mb-8`}
+      >
         <div className='h-full relative flex items-center justify-between p-2'>
           <input
             onChange={(e) => handleInput(e)} 
